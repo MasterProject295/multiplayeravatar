073814335e6a9a515f340cacc434ddd810cbcdd4
RekhaShankarReddy <rekha.shankar.1409@gmail.com>
2017-03-03 13:55:29 -0800

    first commit

---
+++ Assets/Simple Multiplayer/My Scripts/Health.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public const int maxHealth = 100;
+	[SyncVar (hook="OnChangeHealth")] public int currentHealth = maxHealth;
+	public RectTransform healthbar;
+	public bool destroyOnDeath;
+	private NetworkStartPosition[] spawnPoints;
+
+
+	void Start(){
+		if (isLocalPlayer) {
+			spawnPoints = FindObjectsOfType<NetworkStartPosition> ();
+		}
+	}
+
+	public void TakeDamage(int amount){
+
+		if (!isServer) {
+			return;
+		}
+		currentHealth -= amount;
+		if (currentHealth <= 0) {
+
+			if (destroyOnDeath) {
+				Destroy (gameObject);
+			} else {
+				currentHealth = maxHealth;
+				RpcRespawn ();
+			}
+
+		}
+			
+	}
+
+	void OnChangeHealth(int health){
+
+		healthbar.sizeDelta = new Vector2 (health * 2, healthbar.sizeDelta.y);
+	}
+		
+	[ClientRpc]
+	void RpcRespawn(){
+		if(isLocalPlayer){
+			//transform.position = Vector3.zero;
+
+			Vector3 spawnPoint = Vector3.zero;
+
+			if (spawnPoints != null && spawnPoints.Length > 0) {
+				spawnPoint = spawnPoints [Random.Range (0, spawnPoints.Length)].transform.position;	
+			}
+
+			transform.position = spawnPoint;
+		}
+	}
+	
+}
---
+++ Assets/Simple Multiplayer/My Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isLocalPlayer) {
+			return;
+		}
+
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			CmdFire();
+		}
+		
+	}
+
+	[Command]
+	void CmdFire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(bulletPrefab,bulletSpawn.position,bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward*6.0f;
+
+		//Spawn the bullet on clients
+		NetworkServer.Spawn(bullet);
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+}
